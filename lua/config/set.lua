aa1d75a23d2879d74e04e0bd37ded38a5a885ba5
vidarpellving <vidar.pellving@gmail.com>
2023-07-03 02:43:35 +0200

    nvim config

---
+++ lua/config/set.lua
@@ -0,0 +1,27 @@
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.updatetime = 50
+
+vim.opt.scrolloff = 8
+
+vim.g.mapleader = " "
